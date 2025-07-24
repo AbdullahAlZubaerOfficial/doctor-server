9e45a0eb3612a15345ca8c6078719c9c99161dde
Zubaer3002 <your-email@example.com>
2025-07-24 18:17:50 +0600

    up

--- api/index.js
+++ api/index.js
@@ -250,13 +250,12 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-     module.export = app;
+     module.exports = app;
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 
